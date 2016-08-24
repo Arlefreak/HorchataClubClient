8802b81551c52cab441e6390a6a89586a660430b
Arlefreak <hi@arlefreak.com>
2016-08-24 16:32:23 -0500

    Added github to footer, all elements now have target blank

--- src/js/components/footer.jsx
+++ src/js/components/footer.jsx
@@ -8,7 +8,7 @@ class Footer extends React.Component {
                     <li><a href="mailto:horchataclub@gmail.com" className="button"><i className="fa fa-envelope-o"></i></a></li>
                     <li><a href="https://twitter.com/horchata_club" className="button"><i className="fa fa-twitter"></i></a></li>
                     <li><a href="https://instagram.com/horchataclub/" className="button"><i className="fa fa-instagram"></i></a></li>
-                    <li><a href="https://www.facebook.com/" className="button"><i className="fa fa-facebook-official"></i></a></li>
+                    <li><a href="https://github.com/Arlefreak/HorchataClubClient" className="button"><i className="fa fa-github"></i></a></li>
                 </ul>
             </footer>
         );
